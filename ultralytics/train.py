57eff14d9daa0909d168754bc3294557de77b0a7
githupdebaba <110916854+githupdebaba@users.noreply.github.com>
2024-08-01 20:31:20 +0800

    Update train.py

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    model = YOLO('yolov8-AFPN.yaml')  # build from YAML and transfer weights
+    model = YOLO('yolov8.yaml')  # build from YAML and transfer weights
 
     # Train the model
-    results = model.train(data='Sport_data.yaml', epochs=300, imgsz=640, batch=8,)
+    results = model.train(data='coco.yaml', epochs=300, imgsz=640, batch=8,)
 
     # # Load a model
     # model = YOLO('F:/yolov8/ultralytics/runs/detect/train32/weights/last.pt')
